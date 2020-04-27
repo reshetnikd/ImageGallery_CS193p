a48d8582e9f483c086250d86cb4920f9c8765287
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-27 21:21:46 +0300

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Dmitry Reshetnik on 27.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
