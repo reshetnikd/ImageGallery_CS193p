590fe42a9f8307903f11a81f153afbb803276690
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-27 21:38:38 +0300

    Add template UI and data structure

---
+++ ImageGallery/ImageGallery.swift
@@ -0,0 +1,19 @@
+//
+//  ImageGallery.swift
+//  ImageGallery
+//
+//  Created by Dmitry Reshetnik on 27.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+struct ImageGallery {
+    struct Image {
+        let url: URL
+        let aspectRatio: CGFloat
+    }
+    
+    var name: String
+    var images: [Image]
+}
