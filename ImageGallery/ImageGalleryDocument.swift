349136044a02f6ebd717f92649374992d503c8a8
reshetnikd <dmitry.reshetnik@icloud.com>
2020-08-24 15:46:57 +0300

    Add UIDocumentBrowserViewController with supporting files and configuration

---
+++ ImageGallery/ImageGalleryDocument.swift
@@ -0,0 +1,33 @@
+//
+//  ImageGalleryDocument.swift
+//  ImageGallery
+//
+//  Created by Dmitry Reshetnik on 24.08.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryDocument: UIDocument {
+    var thumbnail: UIImage?
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+        
+    }
+    
+    override func fileAttributesToWrite(to url: URL, for saveOperation: UIDocument.SaveOperation) throws -> [AnyHashable : Any] {
+        var attributes = try super.fileAttributesToWrite(to: url, for: saveOperation)
+        
+        if let thumbnail = self.thumbnail {
+            attributes[URLResourceKey.thumbnailDictionaryKey] = [URLThumbnailDictionaryItem.NSThumbnail1024x1024SizeKey : thumbnail]
+        }
+        
+        return attributes
+    }
+}
